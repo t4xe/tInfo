6e217fe99050b06d83ef32298f250168e238d64f
t4xe <71047162+t4xe@users.noreply.github.com>
2021-08-10 20:51:56 +0300

    Updated source code to v1.2

--- main.py
+++ main.py
@@ -2,28 +2,22 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from platform import node, machine, processor, platform, system, version
 from datetime import datetime
 import psutil
-
+    
 class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
         Form.resize(541, 328)
         Form.setMaximumSize(541, 328)
         Form.setMinimumSize(541, 328)
+        
         self.cpuDetailsLabel = QtWidgets.QLabel(Form)
-        self.cpuDetailsLabel.setGeometry(QtCore.QRect(10, 21, 101, 21))
-        font = QtGui.QFont()
-        font.setFamily("Sitka Banner")
-        font.setPointSize(14)
-        self.cpuDetailsLabel.setFont(font)
+        self.cpuDetailsLabel.setGeometry(QtCore.QRect(10, 25, 101, 21))
         self.cpuDetailsLabel.setObjectName("cpuDetailsLabel")
+        
         self.cpuBaseFreqLabel = QtWidgets.QLabel(Form)
         self.cpuBaseFreqLabel.setGeometry(QtCore.QRect(10, 50, 131, 31))
-        
-        font = QtGui.QFont()
-        font.setFamily("Sitka Banner")
-        font.setPointSize(12)
-        self.cpuBaseFreqLabel.setFont(font)
         self.cpuBaseFreqLabel.setObjectName("cpuBaseFreqLabel")
+        
         self.cpuBaseFreqLine = QtWidgets.QLineEdit(Form)
         self.cpuBaseFreqLine.setGeometry(QtCore.QRect(140, 60, 125, 16))
         self.cpuBaseFreqLine.setReadOnly(True)
@@ -31,58 +25,30 @@ class Ui_Form(object):
         
         self.cpuMaxFreqLabel = QtWidgets.QLabel(Form)
         self.cpuMaxFreqLabel.setGeometry(QtCore.QRect(10, 85, 103, 23))
-        font = QtGui.QFont()
-        font.setFamily("Sitka Banner")
-        font.setPointSize(12)
-        self.cpuMaxFreqLabel.setFont(font)
         self.cpuMaxFreqLabel.setObjectName("cpuMaxFreqLabel")
         
         self.cpuMinFreqLabel = QtWidgets.QLabel(Form)
         self.cpuMinFreqLabel.setGeometry(QtCore.QRect(10, 110, 111, 31))
-        font = QtGui.QFont()
-        font.setFamily("Sitka Banner")
-        font.setPointSize(12)
-        self.cpuMinFreqLabel.setFont(font)
         self.cpuMinFreqLabel.setObjectName("cpuMinFreqLabel")
         
         self.cpuNphysLabel = QtWidgets.QLabel(Form)
         self.cpuNphysLabel.setGeometry(QtCore.QRect(10, 150, 131, 31))
-        font = QtGui.QFont()
-        font.setFamily("Sitka Banner")
-        font.setPointSize(12)
-        self.cpuNphysLabel.setFont(font)
         self.cpuNphysLabel.setObjectName("cpuNphysLabel")
         
         self.cpuNlogLabel = QtWidgets.QLabel(Form)
         self.cpuNlogLabel.setGeometry(QtCore.QRect(10, 185, 121, 21))
-        font = QtGui.QFont()
-        font.setFamily("Sitka Banner")
-        font.setPointSize(12)
-        self.cpuNlogLabel.setFont(font)
         self.cpuNlogLabel.setObjectName("cpuNlogLabel")
         
         self.cpuCurrUtilLabel = QtWidgets.QLabel(Form)
         self.cpuCurrUtilLabel.setGeometry(QtCore.QRect(10, 235, 111, 21))
-        font = QtGui.QFont()
-        font.setFamily("Sitka Banner")
-        font.setPointSize(12)
-        self.cpuCurrUtilLabel.setFont(font)
         self.cpuCurrUtilLabel.setObjectName("cpuCurrUtilLabel")
         
         self.cpuCurrPcpuLabel = QtWidgets.QLabel(Form)
         self.cpuCurrPcpuLabel.setGeometry(QtCore.QRect(10, 260, 121, 31))
-        font = QtGui.QFont()
-        font.setFamily("Sitka Banner")
-        font.setPointSize(12)
-        self.cpuCurrPcpuLabel.setFont(font)
         self.cpuCurrPcpuLabel.setObjectName("cpuCurrPcpuLabel")
         
         self.cpuProcTypeLabel = QtWidgets.QLabel(Form)
         self.cpuProcTypeLabel.setGeometry(QtCore.QRect(10, 285, 101, 41))
-        font = QtGui.QFont()
-        font.setFamily("Sitka Banner")
-        font.setPointSize(12)
-        self.cpuProcTypeLabel.setFont(font)
         self.cpuProcTypeLabel.setObjectName("cpuProcTypeLabel")
         
         self.cpuMaxFreqLine = QtWidgets.QLineEdit(Form)
@@ -109,6 +75,7 @@ class Ui_Form(object):
         self.cpuCurrUtilLine.setGeometry(QtCore.QRect(140, 240, 125, 16))
         self.cpuCurrUtilLine.setReadOnly(True)
         self.cpuCurrUtilLine.setObjectName("cpuCurrUtilLine")
+        
         self.cpuCurrPcpuLine = QtWidgets.QLineEdit(Form)
         self.cpuCurrPcpuLine.setGeometry(QtCore.QRect(140, 270, 125, 16))
         self.cpuCurrPcpuLine.setReadOnly(True)
@@ -120,11 +87,7 @@ class Ui_Form(object):
         self.cpuProcTypeLine.setObjectName("cpuProcTypeLine")
         
         self.ramDetailsLabel = QtWidgets.QLabel(Form)
-        self.ramDetailsLabel.setGeometry(QtCore.QRect(290, 21, 101, 21))
-        font = QtGui.QFont()
-        font.setFamily("Sitka Banner")
-        font.setPointSize(14)
-        self.ramDetailsLabel.setFont(font)
+        self.ramDetailsLabel.setGeometry(QtCore.QRect(290, 25, 101, 21))
         self.ramDetailsLabel.setObjectName("ramDetailsLabel")
         
         self.ramRamUsageLine = QtWidgets.QLineEdit(Form)
@@ -139,26 +102,14 @@ class Ui_Form(object):
         
         self.ramUsedRamLabel = QtWidgets.QLabel(Form)
         self.ramUsedRamLabel.setGeometry(QtCore.QRect(290, 110, 91, 31))
-        font = QtGui.QFont()
-        font.setFamily("Sitka Banner")
-        font.setPointSize(12)
-        self.ramUsedRamLabel.setFont(font)
         self.ramUsedRamLabel.setObjectName("ramUsedRamLabel")
         
         self.ramAvaRamLabel = QtWidgets.QLabel(Form)
         self.ramAvaRamLabel.setGeometry(QtCore.QRect(290, 85, 101, 21))
-        font = QtGui.QFont()
-        font.setFamily("Sitka Banner")
-        font.setPointSize(12)
-        self.ramAvaRamLabel.setFont(font)
         self.ramAvaRamLabel.setObjectName("ramAvaRamLabel")
         
         self.ramRamUsageLabel = QtWidgets.QLabel(Form)
-        self.ramRamUsageLabel.setGeometry(QtCore.QRect(290, 145, 81, 21))
-        font = QtGui.QFont()
-        font.setFamily("Sitka Banner")
-        font.setPointSize(12)
-        self.ramRamUsageLabel.setFont(font)
+        self.ramRamUsageLabel.setGeometry(QtCore.QRect(290, 145, 81, 22))
         self.ramRamUsageLabel.setObjectName("ramRamUsageLabel")
         
         self.ramAvaRamLine = QtWidgets.QLineEdit(Form)
@@ -173,50 +124,26 @@ class Ui_Form(object):
         
         self.ramTotalRamLabel = QtWidgets.QLabel(Form)
         self.ramTotalRamLabel.setGeometry(QtCore.QRect(290, 50, 81, 31))
-        font = QtGui.QFont()
-        font.setFamily("Sitka Banner")
-        font.setPointSize(12)
-        self.ramTotalRamLabel.setFont(font)
         self.ramTotalRamLabel.setObjectName("ramTotalRamLabel")
         
         self.generalInformationLabel = QtWidgets.QLabel(Form)
-        self.generalInformationLabel.setGeometry(QtCore.QRect(290, 180, 161, 21))
-        font = QtGui.QFont()
-        font.setFamily("Sitka Banner")
-        font.setPointSize(14)
-        self.generalInformationLabel.setFont(font)
+        self.generalInformationLabel.setGeometry(QtCore.QRect(290, 184, 181, 21))
         self.generalInformationLabel.setObjectName("generalInformationLabel")
         
         self.pcNwNameLabel = QtWidgets.QLabel(Form)
         self.pcNwNameLabel.setGeometry(QtCore.QRect(290, 209, 121, 31))
-        font = QtGui.QFont()
-        font.setFamily("Sitka Banner")
-        font.setPointSize(12)
-        self.pcNwNameLabel.setFont(font)
         self.pcNwNameLabel.setObjectName("pcNwNameLabel")
         
         self.machineTypeLabel = QtWidgets.QLabel(Form)
         self.machineTypeLabel.setGeometry(QtCore.QRect(290, 234, 111, 41))
-        font = QtGui.QFont()
-        font.setFamily("Sitka Banner")
-        font.setPointSize(12)
-        self.machineTypeLabel.setFont(font)
         self.machineTypeLabel.setObjectName("machineTypeLabel")
         
         self.platformTypeLabel = QtWidgets.QLabel(Form)
-        self.platformTypeLabel.setGeometry(QtCore.QRect(290, 259, 91, 51))
-        font = QtGui.QFont()
-        font.setFamily("Sitka Banner")
-        font.setPointSize(12)
-        self.platformTypeLabel.setFont(font)
+        self.platformTypeLabel.setGeometry(QtCore.QRect(290, 259, 101, 51))
         self.platformTypeLabel.setObjectName("platformTypeLabel")
         
         self.osDetailsLabel = QtWidgets.QLabel(Form)
         self.osDetailsLabel.setGeometry(QtCore.QRect(290, 300, 81, 21))
-        font = QtGui.QFont()
-        font.setFamily("Sitka Banner")
-        font.setPointSize(12)
-        self.osDetailsLabel.setFont(font)
         self.osDetailsLabel.setObjectName("osDetailsLabel")
         
         self.pcNwNameLine = QtWidgets.QLineEdit(Form)
@@ -240,14 +167,15 @@ class Ui_Form(object):
         self.osDetailsLine.setObjectName("osDetailsLine")
         
         self.dateLabel = QtWidgets.QLabel(Form)
-        font = QtGui.QFont()
-        font.setFamily("Sitka Banner")
-        font.setPointSize(12)
-        self.dateLabel.setFont(font)
-        self.dateLabel.setGeometry(QtCore.QRect(5, 2, 140, 21))     
-        self.dateLabel.setObjectName("dateLabel")     
+        self.dateLabel.setGeometry(QtCore.QRect(440, 5, 140, 21))     
+        self.dateLabel.setObjectName("dateLabel") 
+
+        self.refreshButton = QtWidgets.QPushButton(Form)
+        self.refreshButton.setGeometry(QtCore.QRect(5, 5, 80, 21))
+        self.refreshButton.setObjectName("refreshButton")
 
         self.lineEditContent()
+        self.refreshButton.clicked.connect(self.lineEditContent)
 
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
@@ -273,11 +201,12 @@ class Ui_Form(object):
         self.ramRamUsageLabel.setText(_translate("Form", "RAM Usage:"))
         self.ramTotalRamLabel.setText(_translate("Form", "Total RAM:"))
         self.generalInformationLabel.setText(_translate("Form", "General Information:"))
-        self.pcNwNameLabel.setText(_translate("Form", "PC Netwok Name:"))
+        self.pcNwNameLabel.setText(_translate("Form", "PC Net. Name:"))
         self.machineTypeLabel.setText(_translate("Form", "Machine Type:"))
         self.platformTypeLabel.setText(_translate("Form", "Platform Type:"))
         self.osDetailsLabel.setText(_translate("Form", "OS Details:"))
         self.dateLabel.setText(_translate("Form", "Date: " + currentDate)) 
+        self.refreshButton.setText(_translate("Form", "Refresh"))
         
     def lineEditContent(self):
         cpuCurrFreq = str(psutil.cpu_freq().current)
@@ -289,6 +218,8 @@ class Ui_Form(object):
         
         cpuCurrUtil = str(psutil.cpu_percent(interval=1))
         cpuCurrPcpu = str(psutil.cpu_percent(interval=1, percpu=True))
+        cpuCurrPcpu = cpuCurrPcpu.replace("[", "")
+        cpuCurrPcpu = cpuCurrPcpu.replace("]", "")
         
         ramTotal = round(psutil.virtual_memory().total/1000000000, 2) - 1
         ramTotalRam = ("{0} GB".format(ramTotal))
@@ -322,13 +253,15 @@ class Ui_Form(object):
         self.machineTypeLine.setText(machineType)
         self.cpuProcTypeLine.setText(processorType)
         self.platformTypeLine.setText(platformType)
-        self.osDetailsLine.setText(osDetails)
-        
+        self.osDetailsLine.setText(osDetails)    
+     
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
+    app.setStyleSheet(open("styleSheet.qss", "r").read())
     Form = QtWidgets.QWidget()
     ui = Ui_Form()
     ui.setupUi(Form)
     Form.show()
     sys.exit(app.exec_())
+
